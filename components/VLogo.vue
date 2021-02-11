21f79aa19e9f494131eeca528340742a1231a68d
Martín Stanicio <martuytimo@gmail.com>
2021-02-11 10:56:07 -0300

    refactor: move `VLogo` component from `VHeader` component to homepage

---
+++ components/VLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center lg:justify-start">
+    <h1 class="font-script text-5xl text-blue-900">LandOff</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VLogo",
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <main class="bg-blue-50 min-h-screen">
-    <VHeader />
-  </main>
+  <div
+    class="bg-blue-50 min-h-screen p-6 flex flex-col gap-12 md:gap-16 md:p-8 lg:px-10"
+  >
+    <VLogo />
+    <VHeader class="lg:px-16" />
+  </div>
 </template>
